7d14ea6b78464cafcb0462be3b36ac6a0b5dd9ce
Omar <omar.sk80@gmail.com>
2024-08-07 20:26:05 -0400

    Install VUEjs

---
+++ resources/js/components/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Not Found</h1>
+    <br>
+    <router-link to="/">Volver iNICIO</router-link>
+</template>
---
+++ resources/js/components/invoices/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>index invoice page</h1>
+</template>
